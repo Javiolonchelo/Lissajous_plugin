5a58c628608b396b35d3dfb010c0fd72d20e88bf
Javier Rodrigo López <javiolonchelo@gmail.com>
2024-12-12 02:37:34 +0100

    Changed submodule strategy, now we use FetchContent. Also Added funny DVD logo and clang-format file

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -3,31 +3,33 @@
 #include "PluginProcessor.h"
 
 //==============================================================================
-AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor(
-    AudioPluginAudioProcessor& p)
+AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor& p)
     : AudioProcessorEditor(&p), processorRef(p) {
-  juce::ignoreUnused(processorRef);
-  // Make sure that before the constructor has finished, you've set the
-  // editor's size to whatever you need it to be.
-  setSize(400, 300);
+    juce::ignoreUnused(processorRef);
+
+	mainWindowWidth = 600;
+	mainWindowHeight = int(mainWindowWidth * 9 / 16);
+
+	DVD_asset = juce::ImageCache::getFromMemory(BinaryData::dvd_png, BinaryData::dvd_pngSize);
+	DVD_width = 111;
+	DVD_height = 50;
+	DVD_position = juce::Point<int>(rand() % (mainWindowWidth - DVD_width), rand() % (mainWindowHeight - DVD_height));
+	directionPositive[0] = true;
+	directionPositive[1] = true;
+	startTimer(int(1000 / 60));
+	setSize(mainWindowWidth, mainWindowHeight);
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor() {}
 
 //==============================================================================
 void AudioPluginAudioProcessorEditor::paint(juce::Graphics& g) {
-  // (Our component is opaque, so we must completely fill the background with a
-  // solid colour)
-  g.fillAll(
-      getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 
-  g.setColour(juce::Colours::white);
-  g.setFont(15.0f);
-  g.drawFittedText("Hello World!", getLocalBounds(),
-                   juce::Justification::centred, 1);
+    g.setColour(juce::Colours::white);
+    g.setFont(15.0f);
+    g.drawFittedText("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+	g.drawImage(DVD_asset, DVD_position.getX(), DVD_position.getY(), DVD_width, DVD_height, 0, 0, DVD_asset.getWidth(), DVD_asset.getHeight());
 }
 
-void AudioPluginAudioProcessorEditor::resized() {
-  // This is generally where you'll want to lay out the positions of any
-  // subcomponents in your editor..
-}
+void AudioPluginAudioProcessorEditor::resized() {}
--- src/PluginEditor.h
+++ src/PluginEditor.h
@@ -3,20 +3,35 @@
 #include "PluginProcessor.h"
 
 //==============================================================================
-class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor
-{
-public:
-    explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
+class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor, private juce::Timer {
+   public:
+    explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor&);
     ~AudioPluginAudioProcessorEditor() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics&) override;
     void resized() override;
 
-private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
+   private:
     AudioPluginAudioProcessor& processorRef;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
+    // Assets
+    juce::Image DVD_asset;
+	int mainWindowWidth, mainWindowHeight, DVD_width, DVD_height;
+	juce::Point<int> DVD_position;
+	bool directionPositive[2];
+
+    // Timer callback
+    void timerCallback() final {
+		DVD_position += juce::Point<int>(directionPositive[0] ? 1 : -1, directionPositive[1] ? 1 : -1);
+		if (DVD_position.getX() > getWidth() - DVD_width || DVD_position.getX() < 0) {
+			directionPositive[0] = !directionPositive[0];
+		}
+		if (DVD_position.getY() > getHeight() - DVD_height || DVD_position.getY() < 0) {
+			directionPositive[1] = !directionPositive[1];
+		}
+		repaint();
+    }
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
 };
