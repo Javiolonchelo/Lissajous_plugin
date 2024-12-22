ec6836eea9110cc0421d31e6667adae4830ac5bb
Javier Rodrigo López <javiolonchelo@gmail.com>
2024-12-22 18:51:24 +0100

    Added params support in a different header

---
+++ src/PluginUtilities.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <JuceHeader.h>
+
+namespace params {
+const juce::ParameterID beam_x = "beam x";
+const juce::ParameterID beam_y = "beam y";
+const juce::ParameterID bypass = "bypass";
+}  // namespace params
